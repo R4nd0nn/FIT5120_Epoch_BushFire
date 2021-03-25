3d4a6de80d98bd91a22b55f30152c1f6b6550a7c
30369916 <jjin0018@student.monash.edu>
2021-03-25 22:13:40 +1100

    2021.3.25 22:13

--- FIT5120_Epoch_BushFire/App_Start/RouteConfig.cs
+++ FIT5120_Epoch_BushFire/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace FIT5120_Epoch_BushFire
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
