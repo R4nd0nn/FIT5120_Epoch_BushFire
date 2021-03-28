902d73c1e4543d362c60988939b2d6332d59fc55
30369916 <jjin0018@student.monash.edu>
2021-03-29 00:09:03 +1100

    3.29 0.08 send_grid key change

--- FIT5120_Epoch_BushFire/SendEmail/Email.cs
+++ FIT5120_Epoch_BushFire/SendEmail/Email.cs
@@ -8,12 +8,12 @@ namespace FIT5120_Epoch_BushFire.SendEmail
     public class Email
     {
         // Please use your API KEY here.
-        private const String API_KEY = "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";
+        private const String API_KEY = "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";
 
         public void Send(String toEmail, String subject, String contents)
         {
             var client = new SendGridClient(API_KEY);
-            var from = new EmailAddress("fit5120ma28@gmail.com","MA28Epoch");
+            var from = new EmailAddress("ma28fit5120@gmail.com","MA28Epoch");
             var to = new EmailAddress(toEmail);
             var plainTextContent = contents;
             var htmlContent = "<p>" + contents + "</p>";
