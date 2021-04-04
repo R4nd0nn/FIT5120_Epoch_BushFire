848c882b4f16800caa95983cb843429052dcc4f9
cgad0001 <81156516+cgad0001@users.noreply.github.com>
2021-04-04 21:25:24 +1000

    Better exception handling and logging (#2)
    Added better exception handling
    Added logging for error tracking
    Updated requirements.txt

--- Prediction_API/app.py
+++ Prediction_API/app.py
@@ -1,27 +1,35 @@
 import flask
-import json
 from flask import request
+import json
 import forecast
+import logging
 
 
 app = flask.Flask(__name__)
+logging.basicConfig(filename='record.log', level=logging.DEBUG,
+    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 @app.route('/forecast')
 def get_weather():
     locality = request.args.get("locality")
     if locality is None:
-        return "Invalid request. No locality found in request query parameters.", 400
+        response = flask.Response("Bad request. No locality found in request query parameters.", 400)
 
     try:
         result = forecast.get_fire_danger_forecast(locality)
+        response = flask.Response(json.dumps(result, indent=1), 200)
 
-    except ValueError:
-        return "Sorry. Locality not found in our database.", 404
+    except LookupError:
+        response = flask.Response("Sorry. Locality not found in our database.", 404)
 
-    except:
-        return "Something went wrong internally.", 500
+    except RuntimeError as e:
+        response = flask.Response(str(e), 500)
 
-    return result, 200
+    except Exception as e:
+        response =  flask.Response(str(e), 500)
+
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
 
 
 if __name__ == "__main__":
